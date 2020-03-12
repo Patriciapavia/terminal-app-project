612ad19d8eaa5edc41ce83863749b5bce84bfc42
patricia <patriciapavia236@gmail.com>
2020-03-12 14:56:14 +1100

    add more gem to print out in the bux

--- loan_calcalate.rb
+++ loan_calcalate.rb
@@ -1,6 +1,8 @@
 
 require 'colorize'
-require 'gosu'
+require "tty-prompt"
+require "tty-box"
+prompt = TTY::Prompt.new
 
 def ask(prompt)
     puts prompt
@@ -9,14 +11,13 @@ def ask(prompt)
 end
 
 
+print TTY::Box.frame "welcome to loan calculation", "lets get started!"
 
-puts"welcome to loan calculation".colorize(:light_blue)
-puts "lets get started" .colorize(:light_blue) 
 name = ask("what is your name".colorize(:light_blue))
 puts "Hello #{name}".colorize(:green)
-income = ask("What is your income weekly").to_f
-puts "great!, now just need some information about your expense".colorize(:color => :light_blue)
-     
+income = ask("What is your income weekly".colorize(:light_blue)).to_f
+print TTY::Box.frame "great!, now just need some information about your expense"
+
 rent = ask("how much is your rent weekly?".colorize(:green)).to_f
 
 food = ask("how much is your food cost weekly?".colorize(:green)).to_f
@@ -29,26 +30,27 @@ loan_need = ask("how much loan you like to apply".colorize(:green)).to_f
 
 expense = transport + food +rent + credit_card_repayment  
 
-puts "your total expense are #{expense}".colorize(:light_blue ).colorize(:light_blue)
+print TTY::Box.frame "your total expense are #{expense}"
 total_fund_after_expense = income - expense  
-
-puts "your fund after expense are #{total_fund_after_expense}".colorize(:light_blue)
-
+print TTY::Box.frame "your fund after expense are #{total_fund_after_expense}"
 able_to_pay_back_weekly = total_fund_after_expense * 30 / 100   
 
+print TTY::Box.frame "your ability to pay back weekly are #{able_to_pay_back_weekly}"
 
-puts "your ability to pay back weekly are #{able_to_pay_back_weekly}".colorize(:light_blue)
    
+
 if(able_to_pay_back_weekly>=250 && loan_need <= 50000) 
-    puts "you are eligible for $#{loan_need}"
+print TTY::Box.frame "you are eligible for $#{loan_need}".colorize(:red)
+   
 elsif(able_to_pay_back_weekly >= 80 ) && (loan_need <= 5000) || (able_to_pay_back_weekly >= 80  && loan_need <= 10000)
- puts "you are eligible for #{loan_need} "
+    print TTY::Box.frame "you are eligible for $#{loan_need}".colorize(:red)
 elsif(able_to_pay_back_weekly >= 100) && (loan_need <= 12000)
-    puts "you are eligible for $#{loan_need}"
+    print TTY::Box.frame "you are eligible for $#{loan_need}".colorize(:red)
 elsif (able_to_pay_back_weekly >= 150 && loan_need <= 15000)
- puts "you are eligible for $#{loan_need}"
+    print TTY::Box.frame "you are eligible for $#{loan_need}".colorize(:red)
 elsif (able_to_pay_back_weekly < 80)
-    puts "sorry maybe try again next time"
+    print TTY::Box.frame "sorry maybe try again next time".colorize(:red)
+    
 end
                                                                
 
